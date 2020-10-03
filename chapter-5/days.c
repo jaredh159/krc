5704adf6f72bbb5297cb754896ad57f9f32a2aab
Jared Henderson <jared@netrivet.com>
2020-10-03 07:59:37 -0400

    ex 5-9

--- chapter-5/days.c
+++ chapter-5/days.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-static char daytab[2][13] = {
+static int daytab[2][13] = {
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
 
@@ -12,6 +12,7 @@ int is_leap(int year)
 int day_of_year(int year, int month, int day)
 {
   int i, leap = is_leap(year);
+  int *p;
 
   if (month < 1 || month > 12)
   {
@@ -19,14 +20,16 @@ int day_of_year(int year, int month, int day)
     return -1;
   }
 
-  if (day < 1 || day > daytab[leap][month])
+  p = daytab[leap];
+
+  if (day < 1 || day > *(p + month))
   {
     printf("Error, invalid day %d for month %d\n", day, month);
     return -1;
   }
 
   for (i = 1; i < month; i++)
-    day += daytab[leap][i];
+    day += *(p + i);
   return day;
 }
 
@@ -41,8 +44,11 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
   }
 
   int i, leap = is_leap(year);
-  for (i = 1; yearday > daytab[leap][i]; i++)
-    yearday -= daytab[leap][i];
+  int *p = daytab[leap];
+
+  for (i = 1; yearday > *(p + i); i++)
+    yearday -= *(p + i);
+
   *pmonth = i;
   *pday = yearday;
 }
@@ -50,9 +56,9 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
 int main(void)
 {
   int year = 2020;
-  int yearday = 590;
+  int yearday = 99;
   int month = 2;
-  int day = 38;
+  int day = 18;
   month_day(year, yearday, &month, &day);
   printf("day %d is %d/%d in %d\n", yearday, month, day, year);
   printf("%d/%d/%d is the %d of the year\n", month, day, year, day_of_year(year, month, day));
