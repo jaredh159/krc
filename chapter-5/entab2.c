0db1d160991717171c51a74fdbbb232fd65dfb45
Jared Henderson <jared@netrivet.com>
2020-10-22 08:40:31 -0400

    WIP: sweet TabArgs struct function

--- chapter-5/entab2.c
+++ chapter-5/entab2.c
@@ -10,43 +10,59 @@
 extern int getline_(char *s, int lim);
 int str_is_int(char *str);
 
+struct TabArgs
+{
+  int user_supplied;
+  int auto_start_col;
+  int auto_col_width;
+  int user_stops[MAX_STOPS];
+};
+
+struct TabArgs get_tab_args(int argc, char *argv[]);
+
 /* entab -m +n -- "stop every n columns, starting at column m" */
 int main(int argc, char *argv[])
 {
+  struct TabArgs tabs = get_tab_args(argc, argv);
+  printf("tabs.auto_start_col is: %d\n", tabs.auto_start_col);
+  printf("tabs.auto_col_width is: %d\n", tabs.auto_col_width);
+
+  for (int i = 0; tabs.user_stops[i] != -1; i++)
+    printf("tabs.user_stops[%d] is %d\n", i, tabs.user_stops[i]);
+}
+
+struct TabArgs get_tab_args(int argc, char *argv[])
+{
+  struct TabArgs tabs;
+  tabs.user_supplied = 0;
+  tabs.auto_start_col = 4;
+  tabs.auto_col_width = 4;
+
   /* move past program name */
   argc--;
   argv++;
 
   int i = 0, stop_index = 0;
-  int start_at_column = -1;
-  int stop_every_n_cols = -1;
-  int stops_supplied_from_argv = 0;
-  int stops[MAX_STOPS];
 
-  for (; i < MAX_STOPS; i++)
-    stops[i] = -1;
+  for (i = 0; i < MAX_STOPS; i++)
+    tabs.user_stops[i] = -1;
 
   while (argc > 0)
   {
-    printf("next argv is: %s\n", *argv);
-
     if (**argv == '-')
-      start_at_column = atoi(++(*argv));
+      tabs.auto_start_col = atoi(++(*argv));
     else if (**argv == '+')
-      stop_every_n_cols = atoi(++(*argv));
+      tabs.auto_col_width = atoi(++(*argv));
     else if (str_is_int(*argv))
-      stops[stop_index++] = atoi(*argv);
+      tabs.user_stops[stop_index++] = atoi(*argv);
     else
       printf("unexpected argv: %s, skipping\n", *argv);
-
     argc--;
     argv++;
   }
-  printf("start_at_column is: %d\n", start_at_column);
-  printf("stop_every_n_cols is: %d\n", stop_every_n_cols);
 
-  for (i = 0; stops[i] != -1; i++)
-    printf("stop[%d] is %d\n", i, stops[i]);
+  tabs.user_supplied = tabs.user_stops[0] != -1;
+  return tabs;
 }
 
 int str_is_int(char *str)
