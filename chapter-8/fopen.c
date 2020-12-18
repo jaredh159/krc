55520446d449decd9d3962da4ef91ec6fdd9f54d
Jared Henderson <jared@netrivet.com>
2020-12-18 09:11:09 -0500

    ex 8-2: bit-fields for fopen

--- chapter-8/fopen.c
+++ chapter-8/fopen.c
@@ -11,29 +11,33 @@
 #define BUFSIZ 1024
 #define OPEN_MAX 20
 
-enum _flags
+typedef struct flags
 {
-  _READ = 01,
-  _WRITE = 02,
-  _UNBUF = 04,
-  _EOF = 010,
-  _ERR = 020
-};
+  unsigned int read : 1;
+  unsigned int write : 1;
+  unsigned int unbuf : 1;
+  unsigned int eof : 1;
+  unsigned int err : 1;
+} flags;
 
 typedef struct _iobuf
 {
   int cnt;
   char *ptr;
   char *base;
-  int flag;
+  flags flag;
   int fd;
 } FILE_;
 
 extern FILE_ _iob[OPEN_MAX];
 
-FILE_ _iob[OPEN_MAX] = {{0, (char *)0, (char *)0, _READ, 0},
-                        {0, (char *)0, (char *)0, _WRITE, 1},
-                        {0, (char *)0, (char *)0, _WRITE | _UNBUF, 2}};
+flags stdinFlags = {1, 0, 0, 0, 0};
+flags stdoutFlags = {0, 1, 0, 0, 0};
+flags stderrFlags = {0, 1, 1, 0, 0};
+
+FILE_ _iob[OPEN_MAX] = {{0, (char *)0, (char *)0, {1, 0, 0, 0, 0}, 0},
+                        {0, (char *)0, (char *)0, {0, 1, 0, 0, 0}, 1},
+                        {0, (char *)0, (char *)0, {0, 1, 1, 0, 0}, 2}};
 
 #define stdin_ &_iob[0]
 #define stdout_ &_iob[1]
@@ -42,11 +46,11 @@ FILE_ _iob[OPEN_MAX] = {{0, (char *)0, (char *)0, _READ, 0},
 int _fillbuf(FILE_ *);
 int _flushbuf(int, FILE_ *);
 
-#define getc(p) (--(p)->cnt >= 0 ? (unsigned char)*(p)->ptr++ : _fillbuf(p))
-#define putc(x, p) (--(p)->cnt >= 0 ? *(p)->ptr++ = (x) : _flushbuf((x), p))
+#define getc_(p) (--(p)->cnt >= 0 ? (unsigned char)*(p)->ptr++ : _fillbuf(p))
+#define putc_(x, p) (--(p)->cnt >= 0 ? *(p)->ptr++ = (x) : _flushbuf((x), p))
 
-#define getchar() getc(stdin)
-#define putchar(x) putc((x), stdout)
+#define getchar_() getc(stdin)
+#define putchar_(x) putc((x), stdout)
 
 FILE_ *fopen_(char *name, char *mode)
 {
@@ -55,7 +59,7 @@ FILE_ *fopen_(char *name, char *mode)
   if (*mode != 'r' && *mode != 'w' && *mode != 'a')
     return NULL;
   for (fp = _iob; fp < _iob + OPEN_MAX; fp++)
-    if ((fp->flag & (_READ | _WRITE)) == 0)
+    if (fp->flag.read == 0 && fp->flag.write == 0)
       break;                 /* found free slot */
   if (fp >= _iob + OPEN_MAX) /* no free slots */
     return NULL;
@@ -74,16 +78,29 @@ FILE_ *fopen_(char *name, char *mode)
   fp->fd = fd;
   fp->cnt = 0;
   fp->base = NULL;
-  fp->flag = (*mode == 'r') ? _READ : _WRITE;
+  fp->flag.unbuf = 0;
+  fp->flag.eof = 0;
+  fp->flag.err = 0;
+  if (*mode == 'r')
+  {
+    fp->flag.read = 1;
+    fp->flag.write = 0;
+  }
+  else
+  {
+    fp->flag.read = 0;
+    fp->flag.write = 1;
+  }
   return fp;
 }
 
-/* _fillbuf: allocate and fill input buffer */ int _fillbuf(FILE_ *fp)
+/* _fillbuf: allocate and fill input buffer */
+int _fillbuf(FILE_ *fp)
 {
   int bufsize;
-  if ((fp->flag & (_READ | _EOF | _ERR)) != _READ)
+  if (!fp->flag.read || fp->flag.err || fp->flag.eof)
     return EOF;
-  bufsize = (fp->flag & _UNBUF) ? 1 : BUFSIZ;
+  bufsize = fp->flag.unbuf ? 1 : BUFSIZ;
   if (fp->base == NULL) /* no buffer yet */
     if ((fp->base = (char *)malloc(bufsize)) == NULL)
       return EOF; /* can't get buffer */
@@ -92,9 +109,9 @@ FILE_ *fopen_(char *name, char *mode)
   if (--fp->cnt < 0)
   {
     if (fp->cnt == -1)
-      fp->flag |= _EOF;
+      fp->flag.eof = 1;
     else
-      fp->flag |= _ERR;
+      fp->flag.err = 1;
     fp->cnt = 0;
     return EOF;
   }
@@ -104,5 +121,7 @@ FILE_ *fopen_(char *name, char *mode)
 int main(void)
 {
   FILE_ *file = fopen_("cat.c", "r");
+  printf("%p\n", file);
   printf("fd: %d\n", file->fd);
+  printf("%c\n", getc_(file));
 }
