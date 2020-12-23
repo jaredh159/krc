56c6a0e40d8c6220596fbf98a9c0244ec2f5b817
Jared Henderson <jared@netrivet.com>
2020-12-23 08:58:11 -0500

    ex 8-6: calloc

---
+++ chapter-8/malloc.c
@@ -0,0 +1,127 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef long Align;
+
+union header
+{
+  struct
+  {
+    union header *ptr;
+    unsigned size;
+  } s;
+  Align x;
+};
+
+typedef union header Header;
+
+static Header base;
+static Header *freep = NULL;
+#define NALLOC 1024
+static Header *morecore_(unsigned);
+void free_(void *);
+
+void *malloc_(unsigned nbytes)
+{
+  Header *p, *prevp;
+  unsigned nunits;
+
+  nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+  if ((prevp = freep) == NULL)
+  {
+    base.s.ptr = freep = prevp = &base;
+    base.s.size = 0;
+  }
+  for (p = prevp->s.ptr;; prevp = p, p = p->s.ptr)
+  {
+    if (p->s.size >= nunits)
+    {
+      if (p->s.size == nunits) /* exactly */
+        prevp->s.ptr = p->s.ptr;
+      else
+      { /* allocate tail end */
+        p->s.size -= nunits;
+        p += p->s.size;
+        p->s.size = nunits;
+      }
+      freep = prevp;
+      return (void *)(p + 1);
+    }
+    if (p == freep) /* wrapped around free list */
+      if ((p = morecore_(nunits)) == NULL)
+        return NULL;
+  }
+}
+
+static Header *morecore_(unsigned nu)
+{
+  char *cp, *sbrk(int);
+  Header *up;
+
+  if (nu < NALLOC)
+    nu = NALLOC;
+
+  cp = sbrk(nu * sizeof(Header));
+  if (cp == (char *)-1)
+    return NULL;
+
+  up = (Header *)cp;
+  up->s.size = nu;
+  free_((void *)(up + 1));
+  return freep;
+}
+
+void free_(void *ap)
+{
+  Header *bp, *p;
+  bp = (Header *)ap - 1; /* point to block header */
+  for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+    if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+      break; /* freed block at start or end of arena */
+  if (bp + bp->s.size == p->s.ptr)
+  {
+    bp->s.size += p->s.ptr->s.size;
+    bp->s.ptr = p->s.ptr->s.ptr;
+  }
+  else
+    bp->s.ptr = p->s.ptr;
+  if (p + p->s.size == bp)
+  {
+    p->s.size += bp->s.size;
+    p->s.ptr = bp->s.ptr;
+  }
+  else
+    p->s.ptr = bp;
+  freep = p;
+}
+
+void *calloc_(unsigned int num_objects, unsigned int object_size)
+{
+  unsigned int num_bytes = num_objects * object_size;
+  void *pointer = malloc_(num_bytes);
+  if (pointer == NULL)
+    return NULL;
+
+  void *current = pointer;
+  char *raw_pointer;
+  int i;
+  for (i = 0; i < num_bytes; i++)
+  {
+    raw_pointer = (char *)((long)current + (i * sizeof(char *)));
+    *raw_pointer = 0;
+  }
+  return pointer;
+}
+
+int main(void)
+{
+  char *s1 = malloc_(5);
+  *s1 = 'H';
+  free_(s1);
+  char *str = calloc_(5, sizeof(char *));
+  // char *str = malloc_(5);
+  printf("pointer is %p\n", str);
+  printf("char %d is %d, or %c\n", 0, (int)str[0], str[0]);
+  free_(str);
+  return 0;
+}
